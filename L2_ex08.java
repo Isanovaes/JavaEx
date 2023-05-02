ea1f56c7f47580100dcd262863a012f7d05c403a
Isa Carolina <88831860+Isanovaes@users.noreply.github.com>
2023-05-02 14:44:12 -0300

    Add files via upload
    Fatoração simples de números entre 1 a 16.

---
+++ L2_ex08.java
@@ -0,0 +1,31 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class L2_ex08 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int acumulador = 1;
+		System.out.print("Digite um número e saiba seu fatorial:");
+		int a = sc.nextInt();
+		if (a < 0) {
+			System.out.print("Esse número é inválido");
+		}
+		else {
+			System.out.printf("%d! = ", a); //ex: 5! = 
+		
+			for (int contador = a; contador != 0; contador--) {
+				System.out.printf("%d", contador);
+				if (contador != 1) {
+					System.out.printf("*"); //para ficar bonitinho na tela
+				}// end if
+				acumulador *= contador; //acumulador = acumulador * contador
+			}// end for
+			System.out.printf(" = %d.", acumulador); // por algum motivo funciona ate do 1 ao 16
+		sc.close();
+		
+		}
+	}
+
+}
